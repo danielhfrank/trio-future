394dc8fb516f719b414f0e8c09337458c46e07eb
Dan Frank <danielhfrank@gmail.com>
2020-12-05 14:18:50 -0800

    Rework to what we actually wanted, praise be

--- tests/test_future.py
+++ tests/test_future.py
@@ -1,16 +1,35 @@
-import outcome
+from outcome import Value, Error
+import pytest
 import trio
 
-from trio_future.future import IntFuture
+from trio_future.future import Future
+
 
 async def my_fn():
-    await trio.sleep(1)
+    await trio.sleep(3)
     return 7
 
 
-def test_the_business():
-    async def the_business():
-        async with trio.open_nursery() as nursery:
-            x = await IntFuture.run(my_fn, nursery)
-            assert(x == outcome.Value(7))
-    trio.run(the_business)
+async def get_biffed():
+    await trio.sleep(1)
+    raise RuntimeError("Boom")
+
+
+async def test_the_future():
+    async with trio.open_nursery() as nursery:
+        print("a")
+        fut = Future.run(my_fn, nursery)
+        print("b")
+        func_outcome = await fut.outcome()
+        print("c")
+        assert func_outcome == Value(7)
+
+
+async def test_getting_biffed():
+    async with trio.open_nursery() as nursery:
+        fut = Future.run(get_biffed, nursery)
+        # note that if we had awaited the function, this would just raise an exception
+        func_outcome = await fut.outcome()
+        assert isinstance(func_outcome, Error)
+        with pytest.raises(RuntimeError):
+            func_outcome.unwrap()
--- trio_future/future.py
+++ trio_future/future.py
@@ -1,19 +1,31 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
 import trio
 import outcome
 
 
-class IntFuture:
+@dataclass
+class Future:
+
+    # TODO we can type this!
+    result_chan: trio.abc.ReceiveChannel
+    nursery: trio.Nursery  # TODO not sure if we need this but feels like good practice
+
     @staticmethod
-    async def run(async_fn, nursery: trio.Nursery) -> outcome.Outcome:
+    def run(async_fn, nursery: trio.Nursery) -> Future:
         send_chan, recv_chan = trio.open_memory_channel(0)
 
         async def producer():
-            return_val = await async_fn()
+            return_val = await outcome.acapture(async_fn)
             async with send_chan:
                 await send_chan.send(return_val)
 
         nursery.start_soon(producer)
-        async with recv_chan:
-            # TODO sketchy anext, no idea if this will work
-            x = await recv_chan.__anext__()
-            return outcome.Value(x)
+
+        return Future(recv_chan, nursery)
+
+    async def outcome(self) -> outcome.Outcome:
+        async with self.result_chan:
+            return await self.result_chan.receive()
